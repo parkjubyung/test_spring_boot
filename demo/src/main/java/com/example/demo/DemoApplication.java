6e1ab87ae641b7d185bec7552a71fb934b410d00
parkjubyeong <pjb0730@github.com>
2024-02-15 00:10:49 +0900

    RestAPI Study Get Method
    - RestController
    - RequestMapping
    - GetMapping

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,10 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 @SpringBootApplication
 public class DemoApplication {
 
@@ -10,4 +14,31 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+}
+
+
+public class Coffee {
+	private final String id;
+	private String name;
+
+	public Coffee(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Coffee(String name) {
+		this(UUID.randomUUID().toString(), name);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
---
+++ demo/src/main/java/com/example/demo/controllers/RestApiDemoController.java
@@ -0,0 +1,41 @@
+package com.example.demo.controllers;
+
+import com.example.demo.Coffee;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class RestApiDemoController {
+    private List<Coffee> coffees = new ArrayList<>();
+
+    public RestApiDemoController() {
+        coffees.addAll(List.of(
+                new Coffee("Cafe Cereza"),
+                new Coffee("Cafe Ganador"),
+                new Coffee("Cafe Lareno"),
+                new Coffee("Cafe Tres Pontas")
+        ));
+    }
+
+    @RequestMapping(value = "/coffees", method = RequestMethod.GET)
+    Iterable<Coffee> getCoffees() {
+        return coffees;
+    }
+
+    @GetMapping("/coffees/{id}")
+    Optional<Coffee> getCoffeeById(@PathVariable String id) {
+        for (Coffee c: coffees) {
+            if (c.getId().equals(id)) {
+                return Optional.of(c);
+            }
+        }
+        return Optional.empty();
+    }
+
+
+
+
+}
